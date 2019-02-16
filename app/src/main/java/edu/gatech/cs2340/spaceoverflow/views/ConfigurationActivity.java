6fbc32fa4c5689351307a1546b82be34ac0f0705
Jarod Schneider <jschneider42@gatech.edu>
2019-02-15 19:36:37 -0500

    fixed ConfigurationActivity

--- app/src/main/java/edu/gatech/cs2340/spaceoverflow/views/ConfigurationActivity.java
+++ app/src/main/java/edu/gatech/cs2340/spaceoverflow/views/ConfigurationActivity.java
@@ -4,8 +4,11 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 import edu.gatech.cs2340.spaceoverflow.R;
+import edu.gatech.cs2340.spaceoverflow.viewmodels.ConfigurationViewModel;
 
 public class ConfigurationActivity extends AppCompatActivity {
+
+    private ConfigurationViewModel viewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
