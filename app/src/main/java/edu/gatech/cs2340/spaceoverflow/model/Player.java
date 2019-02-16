ba89966b38ff5d59451e9f4c5cf70367632a7e4d
mcosta33 <mcosta33@gatech.edu>
2019-02-15 19:51:39 -0500

    Player class implemented

--- app/src/main/java/edu/gatech/cs2340/spaceoverflow/model/Player.java
+++ app/src/main/java/edu/gatech/cs2340/spaceoverflow/model/Player.java
@@ -2,10 +2,54 @@ package edu.gatech.cs2340.spaceoverflow.model;
 
 public class Player {
     String name;
-    int PilotSkill;
-    int FighterSkill;
-    int TraderSkill;
-    int EngineerSkill;
+    int pilotSkill;
+    int fighterSkill;
+    int traderSkill;
+    int engineerSkill;
 
+    public String toString() {
+        return "Player " + name + "has skill levels: Pilot(" + pilotSkill + "), " +
+                "Fighter(" + fighterSkill + "), Trader(" + traderSkill + "), and Engineer(" +
+                engineerSkill + ").";
+    }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPilotSkill() {
+        return pilotSkill;
+    }
+
+    public void setPilotSkill(int pilotSkill) {
+        this.pilotSkill = pilotSkill;
+    }
+
+    public int getFighterSkill() {
+        return fighterSkill;
+    }
+
+    public void setFighterSkill(int fighterSkill) {
+        this.fighterSkill = fighterSkill;
+    }
+
+    public int getTraderSkill() {
+        return traderSkill;
+    }
+
+    public void setTraderSkill(int traderSkill) {
+        this.traderSkill = traderSkill;
+    }
+
+    public int getEngineerSkill() {
+        return engineerSkill;
+    }
+
+    public void setEngineerSkill(int engineerSkill) {
+        this.engineerSkill = engineerSkill;
+    }
 }
