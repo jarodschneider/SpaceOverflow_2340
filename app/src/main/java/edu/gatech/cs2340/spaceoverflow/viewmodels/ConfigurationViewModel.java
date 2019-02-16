bc1c3f1ee3a8c17ef94d65e6639a6d43b0e027f0
Sunny Qi <sunny_qi@lawn-128-61-34-147.lawn.gatech.edu>
2019-02-15 19:56:44 -0500

    configure viewModel code outline

--- app/src/main/java/edu/gatech/cs2340/spaceoverflow/viewmodels/ConfigurationViewModel.java
+++ app/src/main/java/edu/gatech/cs2340/spaceoverflow/viewmodels/ConfigurationViewModel.java
@@ -4,11 +4,22 @@ import android.arch.lifecycle.AndroidViewModel;
 import android.app.Application;
 import android.support.annotation.NonNull;
 
+import edu.gatech.cs2340.spaceoverflow.model.Player;
+
 public class ConfigurationViewModel extends AndroidViewModel {
+
+    private Player player;
 
     public ConfigurationViewModel(@NonNull Application application) {
         super(application);
     }
 
+    public boolean validateSkillLevels() {
+
+    }
+
+    public void createPlayer() {
+
+    }
 
 }
