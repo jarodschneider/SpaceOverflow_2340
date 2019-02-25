b79481c4bc1885d317df7fa763e0156ef895a320
Jarod Schneider <jschneider42@gatech.edu>
2019-02-25 17:39:04 -0500

    started M6, implemented randomly generated SolarSystems

---
+++ app/src/main/java/edu/gatech/cs2340/spaceoverflow/model/SolarSystem.java
@@ -0,0 +1,52 @@
+package edu.gatech.cs2340.spaceoverflow.model;
+
+import java.util.Arrays;
+
+public class SolarSystem {
+
+    private String name;
+    private int[] coords;
+    private TechLevel techLevel;
+    private ResourceLevel resourceLevel;
+
+    @Override
+    public String toString() {
+        return "SolarSystem{" +
+                "name='" + name + '\'' +
+                ", coords=" + Arrays.toString(coords) +
+                ", techLevel=" + techLevel +
+                ", resourceLevel=" + resourceLevel +
+                '}';
+    }
+
+    public SolarSystem(String name, int[] coords, TechLevel techLevel, ResourceLevel resourceLevel) {
+        this.name = name;
+        this.coords = coords;
+        this.techLevel = techLevel;
+        this.resourceLevel = resourceLevel;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int[] getCoords() {
+        return coords;
+    }
+
+    public TechLevel getTechLevel() {
+        return techLevel;
+    }
+
+    public void setTechLevel(TechLevel techLevel) {
+        this.techLevel = techLevel;
+    }
+
+    public ResourceLevel getResourceLevel() {
+        return resourceLevel;
+    }
+
+    public void setResourceLevel(ResourceLevel resourceLevel) {
+        this.resourceLevel = resourceLevel;
+    }
+}
--- app/src/main/java/edu/gatech/cs2340/spaceoverflow/viewmodels/ConfigurationViewModel.java
+++ app/src/main/java/edu/gatech/cs2340/spaceoverflow/viewmodels/ConfigurationViewModel.java
@@ -5,12 +5,31 @@ import android.app.Application;
 import android.support.annotation.NonNull;
 import android.util.Log;
 
+import java.util.Random;
+
 import edu.gatech.cs2340.spaceoverflow.model.Player;
+import edu.gatech.cs2340.spaceoverflow.model.ResourceLevel;
+import edu.gatech.cs2340.spaceoverflow.model.SolarSystem;
+import edu.gatech.cs2340.spaceoverflow.model.TechLevel;
 
 public class ConfigurationViewModel extends AndroidViewModel {
 
     private Player player;
     private final int TOTAL_SKILL = 16;
+
+    private static final String SolarSystemName[] = {
+            "Acamar",
+            "Adahn",		// The alternate personality for The Nameless One in "Planescape: Torment"
+            "Aldea",
+            "Andevian",
+            "Antedi",
+            "Balosnee",
+            "Baratas",
+            "Brax",			// One of the heroes in Master of Magic
+            "Bretel",		// This is a Dutch device for keeping your pants up.
+            "Calondia"};
+
+    private SolarSystem[][] universe = new SolarSystem[150][100];
 
 
     public ConfigurationViewModel(@NonNull Application application) {
@@ -46,7 +65,32 @@ public class ConfigurationViewModel extends AndroidViewModel {
 
     public void createPlayer(Player player) {
         this.player = player;
+        createUniverse();
         Log.i("ConfigurationViewModel", "The player was created with 1000 credits and a Gnat spaceship. " + player.toString());
+    }
+
+    private void createUniverse() {
+        for (int i = 0; i < 9; i++) {
+            int coords[] = generateCoords();
+            SolarSystem s = new SolarSystem(
+                    SolarSystemName[i],
+                    coords,
+                    TechLevel.values()[new Random().nextInt(8)],
+                    ResourceLevel.values()[new Random().nextInt(13)]);
+            universe[coords[0]][coords[1]] = s;
+            Log.i("ConfigurationViewModel", "SolarSystem created! " + s.toString());
+        }
+    }
+
+    private int[] generateCoords() {
+        Random rand = new Random();
+        int x = rand.nextInt(universe.length);
+        int y = rand.nextInt(universe[0].length);
+        if (universe[x][y] == null) {
+            return new int[]{x, y};
+        } else {
+            return generateCoords();
+        }
     }
 
 }
